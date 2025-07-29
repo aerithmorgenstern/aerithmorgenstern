485e1914928484b98bf12dbc172a7f92fa89dffa
aerithmorgenstern <aerithmorgenstern@gmail.com>
2025-07-28 17:19:04 -0700

    added tiktok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Footer.css';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../../App.css';
 import GalleryCarousel from '../GalleryCarousel';
 
